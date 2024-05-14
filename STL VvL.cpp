637f91c84d71aee59e6eccb3cc00012f8937a269
User54681 <anzolotylh@gmail.com>
2024-05-14 17:44:37 +0300

    добавила измерение памяти

--- STL VvL.cpp
+++ STL VvL.cpp
@@ -6,22 +6,31 @@
 int main()
 {
     setlocale(LC_ALL, "Rus");
-    std::vector<int> vec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::list<int> list{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    std::vector<int> vec;
+    std::list<int> list;
+
+    for (int i = 0; i < 200000; ++i) {
+        vec.push_back(i);
+        list.push_back(i);
+    }
+
+    std::cout << "Размер вектора: " << sizeof(vec) + (vec.size() * sizeof(int)) << " байт\n";
+    std::cout << "Размер списка: " << sizeof(list) + (list.size() * sizeof(int)) << " байт\n";
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     auto start_vec = std::chrono::high_resolution_clock::now();
-    vec.insert(vec.begin() + vec.size() / 2, 8);
+    vec.insert(vec.begin(), 8);
     auto end_vec = std::chrono::high_resolution_clock::now();
 
     auto start_list = std::chrono::high_resolution_clock::now();
     auto iter{ list.begin() };
-    std::advance(iter, list.size() / 2); 
     list.insert(iter, 8);
     auto end_list = std::chrono::high_resolution_clock::now();
 
-    std::chrono::duration<double, std::milli> vec_time = end_vec - start_vec;
-    std::chrono::duration<double, std::milli> lst_time = end_list - start_list;
+    auto duration_vector = std::chrono::duration_cast<std::chrono::microseconds>(end_vec - start_vec).count();
+    auto duration_list = std::chrono::duration_cast<std::chrono::microseconds>(end_list - start_list).count();
 
-    std::cout << "Время вставки в середину вектора: " << vec_time.count() << " мс\n";
-    std::cout << "Время вставки в середину списока: " << lst_time.count() << " мс\n";
+    std::cout << "Время вставки в вектор: " << duration_vector << " мкс\n";
+    std::cout << "Время вставки в список: " << duration_list << " мкс\n";
 }
